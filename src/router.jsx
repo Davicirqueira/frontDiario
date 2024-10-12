d517afac2dc2b2ed1ded1be54187ec2643d35047
davi <daviforever17@gmail>
2024-10-11 22:43:06 -0300

    security.

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from './pages/home';
+import Login from './pages/login';
 import Consultar from './pages/consultar';
 import Cadastrar from './pages/cadastrar';
 
@@ -11,7 +11,7 @@ export default function Navegacao() {
 
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Login />} />
         <Route path='/consultar' element={<Consultar />} />
         <Route path='/cadastrar' element={<Cadastrar />} />
         <Route path='/cadastrar/:id' element={<Cadastrar />} />
